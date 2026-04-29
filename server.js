7a1059d60837eb93b444de493971b70fb1087396
Aayash <dangaayash@gmail.com>
2026-04-29 13:28:56 +0545

    Initial commit

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+import { connectDB } from './src/config/db.js';
+import { errorHandler, notFound } from './src/middleware/errorMiddleware.js';
+
+import authRoutes from './src/routes/authRoutes.js';
+
+dotenv.config();
+connectDB()
+
+const app = express();
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api/auth', authRoutes);
+
+app.get('/api/health', (req, res) => {
+  res.json({ status: 'FitForge API is running 🏋️' });
+});
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});

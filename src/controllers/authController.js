123b866050876a01d64efe715a32ed335d493180
Aayash <dangaayash@gmail.com>
2026-05-06 11:14:26 +0545

    cookie problem fixed

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -12,7 +12,7 @@ const sendTokenResponse = (user, statusCode, res) => {
   const cookieOptions = {
     httpOnly: true,   
     secure: process.env.NODE_ENV === 'production',  // HTTPS only in prod
-    sameSite: 'strict',
+    sameSite: 'None',
     maxAge: 7 * 24 * 60 * 60 * 1000,  // 7 days in ms
   };
 
